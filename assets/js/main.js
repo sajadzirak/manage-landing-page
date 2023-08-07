92239d3bb71522822a957eaa2b8936a3e7747ba0
sajad <iriafmig29@gmail.com>
2023-08-07 15:54:43 +0330

    mobile view completed

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu')
+const navLinks = document.querySelectorAll('.nav__link')
+
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+        navToggle.style.display = 'none'
+    })
+}
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+        navToggle.style.display = 'block'
+    })
+}
+navLinks.forEach((element) => {
+    element.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+        navToggle.style.display = 'block'
+    })
+})
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header')
+    if(window.scrollY >= 50) {
+        header.style.backgroundColor = 'var(--body-color)'
+        header.style.boxShadow = '0 1px 10px hsla(228, 39%, 23%, 0.238)'
+    }else {
+        header.style.backgroundColor = 'transparent'
+        header.style.boxShadow = 'none'
+    }
+})
+
+// const swiper = new Swiper('.swiper', {
+//     direction: 'vertical',
+//     loop: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// })
